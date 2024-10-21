3a0dd376addee38c56aea50728d1d677f398f89e
Mohammad rasool <muhammedjaf33@gmail.com>
2024-10-21 15:26:01 +0300

    Adding middleware to the logout api

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('login',[AuthController::class,'login']);
-Route::post('logout',[AuthController::class,'logout']);
+Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 Route::post('register',[AuthController::class,'register']);
 Route::apiResource('tasks', TaskController::class)->middleware('auth:sanctum');
